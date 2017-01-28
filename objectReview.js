6558e1d349caca4e12d39a8f70ac21d21553fcd8
MikeB <burton.mj@gmail.com>
2017-01-28 12:06:34 -0700

    mid progress

--- objectReview.js
+++ objectReview.js
@@ -1,19 +1,32 @@
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Your code here
-
+  var favoriteThings = {
+    band:'daft punk',
+    food:'meat',
+    person:'heidi',
+    book:'4HWW',
+    movie:'top gun',
+    holiday:'christmas'
+  };
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+favoriteThings.car = 'tesla';
+favoriteThings.brand = 'lululemon';
 
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
+console.log(favoriteThings);
 
-  //Your code here
+//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
+
+favoriteThings.food = 'Lettuce'
+favoriteThings.book = '50 Shades of Gray';
+
+console.log(favoriteThings);
+
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
-
+window.alert(favoriteThings.person);
+window.alert(favoriteThings.book);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -21,7 +34,7 @@
 
 
 var user = {
-    name: 'Tyler McGinnis', 
+    name: 'Tyler McGinnis',
     email: null,
     pwHash: 'U+Ldlngx2BYQk',
     birthday: undefined,
@@ -47,15 +60,15 @@ var user = {
 
 
 
-//create an empty object called methodCollection. 
+//create an empty object called methodCollection.
 
   //Your code here
 
-//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
+//Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console.
 
   //Your code here
 
-//Now call your alertHello and logHello methods. 
+//Now call your alertHello and logHello methods.
 
   //Your code here
 
@@ -67,6 +80,3 @@ var user = {
 
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
-
-
-
